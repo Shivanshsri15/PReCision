dbfa5038e37dc39670068afbb482638ab5c3ecc3
Suryansh Garg <132171202+SuryanshGarg04@users.noreply.github.com>
2026-04-13 14:34:12 +0530

    Fix email type and add constraint in LoginDto
    Changed email type from int to string and added constraint property.

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,8 +2,9 @@ import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
-  email!: int;
-
+  email!: string;
+  @IsValid()
+  constraint!:string;
   @IsString()
   password!: string;
 }
